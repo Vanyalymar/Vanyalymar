e3d399213f7646802c4e947436b23ef084fb6b20
Vanya <105154363+Vanyalymar@users.noreply.github.com>
2022-05-08 16:46:11 +0500

    Update kal.py

--- kal.py
+++ kal.py
@@ -5,24 +5,29 @@ a = 0
 b = 0
 
 
-vi = str(input("что нужно сделать: "));
+vi = str(input("Что нужно сделать? "));
+
 a = float(input("Введите первое число: "))
+
 b  = float(input("Введите второе число: "))
+
 t =  ('Результат: ')
 
 if vi == '+':
+    
     print (t)
-
     print (a+b)
     
-
 elif vi == '-':
+    
     print(a-b)
     print (t)
+    
 elif vi == '/':
-  
+    
     print(a/b)
     print (t)
+    
 elif vi == '*':
 
     print (t)
