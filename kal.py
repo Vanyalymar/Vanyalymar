f0b214ebabfc7144d7cff74d66eca043d90f0262
Vanyalymar <105154363+Vanyalymar@users.noreply.github.com>
2022-05-08 16:28:27 +0500

    Add files via upload

---
+++ kal.py
@@ -0,0 +1,32 @@
+from pickletools import floatnl
+
+
+vi = 0
+a = 0
+b = 0
+
+
+vi = str(input("что нужно сделать: "));
+a = float(input("Введите первое число: "))
+b  = float(input("Введите второе число: "))
+t =  ('Результат: ')
+
+if vi == '+':
+    print (t)
+
+    print (a+b)
+    
+
+elif vi == '-':
+    print(a-b)
+    print (t)
+elif vi == '/':
+  
+    print(a/b)
+    print (t)
+elif vi == '*':
+
+    print (t)
+    print(a*b)
+
+    
